c5654a0c46215bf9a02839b5cd37ff07b9c4164c
ZWJ <qwrtyp@live.cn>
2021-12-08 11:43:44 +0800

    添加依赖

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const swaggerInstall = require('./utils/swagger');
 const app = new Koa();
 swaggerInstall(app)
 
-app.listen(3001);
+app.listen(3002);
 
-console.log("connect to http://127.0.0.1", 3001);
+console.log("connect to http://127.0.0.1", 3002);
--- routes/test.js
+++ routes/test.js
@@ -1,4 +1,4 @@
-/**,
+/**
  * @swagger
  * /api/addExam:
  *    post:
